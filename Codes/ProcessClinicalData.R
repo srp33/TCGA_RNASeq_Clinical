f8169447ae723f016f93f8ba040e58a190bd6ba7
mumtahena <moom.rahman@utah.edu>
2015-05-26 02:20:50 -0600

    Tue May 26 02:20:50 MDT 2015

--- Codes/ProcessClinicalData.R
+++ Codes/ProcessClinicalData.R
@@ -1,56 +1,59 @@
-
-<<<<<<< HEAD
-##install packackage 'plyr'
-#install.packages("plyr")
-library(plyr)
-=======
 if (!require("plyr")) {
    install.packages("plyr", dependencies = TRUE)
    library(plyr)
    }
->>>>>>> 828998a3b118968577ba27268cb159e71be7411b
 
+data==identifiers=tmp_data=tmp_identifier=NULL
 dirname='.'
 setwd(dirname)#Set the directory where the clinical data is located for each cancer in separate folder
 filenames<-system("ls */nationwidechildrens.org_clinical_patient*", intern=T)
-
-##Identifying only unique column names for all the tumor samples available
-
 for(i in 1:length(filenames)){#####iterating through each of the clinical files to create new matrix files with ALL clinical variables
-  f<-read.delim(paste(c(dirname,filenames[i]), collapse=''), header=1) ###reading in the filess one at a time
-  rownames(f)<-f$bcr_patient_barcode
-  f<-f[3:length(f$bcr_patient_barcode),]
+  print(i)
+  f<-(read.delim(paste(c(dirname,filenames[i]), collapse=''))) ###reading in the filess one at a time
+  tmp_data<-f[3:nrow(f),]
+  tmp_identifier<-f[1:2,]
   if(i==1){
-    data<-f
-  }else{
-    data<-list(data,f)
+    data<-tmp_data
+    identifier<-tmp_identifier
+}else{
+    identifier<-list(identifier,tmp_identifier)
+    identifier<-rbind.fill.matrix(identifier)
+    for(j in 1:ncol(identifier)){
+      if(!is.na(identifier[3,j])){
+        identifier[1,j]<-identifier[3,j]
+        identifier[2,j]<-identifier[4,j]
+      }
+    }
+    identifier<-identifier[1:2,]
+    data<-list(data,tmp_data)
     data<-rbind.fill.matrix(data)
+    #data<-merge(data,f)
   }
 }
-rownames(data)<-data[,1]
+rownames(data)<-data[,2]
+
 #Now, converting short TCGA ids reported in clinical data to long TCGA ids reported in RNA-Seq dataset using R codes
 
-pancan20_featureCounts_5<-as.matrix(read.table("Datasets/PANCAN20.IlluminaHiSeq_RNASeqV2.tumor_Rsubread_FeatureCounts.txt", header=1, row.names=1, nrows=5,sep='\t', check.names = F)) #getting the long TCGA IDs used in RNA-Seq dataset
-sample_names<-colnames(pancan20_featureCounts_5)
+sample_names<-rownames(as.matrix(read.table("PANCAN24_CancerType_Samples.txt", row.names=1, sep='\t', check.names = F))) #getting the long TCGA IDs used in RNA-Seq dataset
 partial_sample_names<-rownames(data)
 counter=0##to check how many replacement has been done
 for (j in 1:length(partial_sample_names)){
-    if(!is.na(pmatch(partial_sample_names[j],sample_names))){
-      partial_sample_names[j]<-sample_names[pmatch(partial_sample_names[j],sample_names, duplicates.ok=F)]  
-      counter=counter+1
-    }
+  if(!is.na(pmatch(partial_sample_names[j],sample_names))){
+    partial_sample_names[j]<-sample_names[pmatch(partial_sample_names[j],sample_names, duplicates.ok=F)]  
+    counter=counter+1
+  }
 }
-#counter##clinical variables available for 6820 of the 7706 tumor samples
+
 rownames(data)<-partial_sample_names
-clinical_data<-matrix(NA, nrow=7706,ncol=420) ###instantiating an NA matrix
-rownames(clinical_data)<-colnames(pancan20_featureCounts_5)
+clinical_data<-matrix(NA, nrow=9264,ncol=548) ###instantiating an NA matrix
+rownames(clinical_data)<-sample_names
 colnames(clinical_data)<-colnames(data)
 for(i in 1:length(rownames(clinical_data))){
   sample_id<-rownames(clinical_data)[i]
   if(sample_id%in%rownames(data)){
-      clinical_data[sample_id,]<-data[sample_id,]      
-    }
+    clinical_data[sample_id,]<-data[sample_id,]      
   }
-##Writing the clinical data file
+}
+clinical_data_identifier<-cbind(t(identifier),t(clinical_data))
+write.table(clinical_data_identifier,file="TCGA_clinical_data_ordered_all_clinical_variables_samples_as_columns.txt", sep='\t',col.names=NA, quote=F)
 
-write.table(t(clinical_data),file="TCGA20_clinical_data_ordered_all_clinical_variables_samples_as_columns.txt", sep='\t',col.names=NA, quote=F)
